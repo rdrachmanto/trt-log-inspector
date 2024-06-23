5d921e8a34e3acc631ba3cf5c6ded6dd1b5210c6
Rakandhiya Daanii Rachmanto <rakandhiya.daanii@gmail.com>
2024-06-23 22:58:59 +0700

    Add line parser and conversion time duration examiner - Add query-able line parser - Add conversion time duration that examines the time taken for each conversion stage - Revamp the docstrings in trt_log_file.py - For now, remove the test_file_helper.py, Will be replaced in future commits

--- src/trt_log_inspector/trt_log_file.py
+++ src/trt_log_inspector/trt_log_file.py
@@ -1,29 +1,75 @@
-from typing import List
+import re
+from typing import Dict, Generator, List, Union
 
 
 class TrtLogFile:
-    def __init__(self, name, path) -> None:
-        self.name = name,
+    """
+    Core class for examining the TensorRT (TRT) log file.
+    """
+
+    def __init__(self, name: str, path: str) -> None:
+        self.name = (name,)
         self.path = path
-        self.content = self._open_file_from_path()
-    
-    def _open_file_from_path(self) -> List[str]:
+
+    def _parse_line(
+        self, query: Union[None, list[re.Pattern]] = None
+    ) -> Generator[str, Union[None, list[re.Pattern]], None]:
         """
-        Open and read contents of the file from the provided path
+        Attempt to open the file from `self.path` and apply query (if specified) to filter the text.
 
         Args:
-        - path (str): The relative path to the file
+        - query: compiled regex pattern to match against.
 
-        Returns:
-        - str: Contents of the file as str
+        Yields:
+        - str: A stripped line from the file that matched the query.
 
         Raises:
-        - FileNotFoundError: If the path specified is not accessible
+        - FileNotFoundError: If file from self.path does not exist.
         """
+
         try:
             with open(self.path) as f:
-                contents = f.read()
-            return contents.splitlines()
+                for line in f:
+                    if query is not None and any(p.search(line) for p in query):
+                        yield line.strip()
+                    elif query is None:
+                        yield line.strip()
         except FileNotFoundError:
             raise FileNotFoundError(f"{self.path} cannot be opened")
 
+    def conversion_duration_info(self) -> List[Dict[str, float]]:
+        """
+        Extract conversion duration information for each stages of conversion.
+
+        Returns:
+        - List[Dict]: Information about each stage and duration (in seconds)
+        """
+
+        stages = [
+            "Formats and tactics selection",
+            "Engine generation",
+            "Calibration",
+            "Configuring builder",
+            "Graph construction and optimization",
+            "Finished engine building",
+        ]
+        re_stages_filter = [
+            re.compile(rf"{pattern}.*?(\d+\.\d+)") for pattern in stages
+        ]
+        line_parser = self._parse_line(query=re_stages_filter)
+
+        results = []
+        for pr in line_parser:
+            detected_stage = ""
+            for s in stages:
+                if s in pr:
+                    detected_stage = s
+
+            results.append(
+                {
+                    "stage": detected_stage,
+                    "duration_s": float(re.findall(r"\d+\.\d+", pr)[0]),  # type: ignore
+                }
+            )
+
+        return results
